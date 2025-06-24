5ec0036b1bb13ab3b6fe3fbc39f2d626762e6234
mahdi <Mahdi.eza121@gmail.com>
2025-06-24 17:55:05 +0330

    front-end setup

---
+++ resources/views/components/layouts/home.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+    @include('partials.head')
+    <body>
+        <x-header>
+        </x-header>
+        {{ $slot }}
+    </body>
+</html>
---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    {{-- Bootstrap --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.rtl.min.css" integrity="sha384-Xbg45MqvDIk1e563NLpGEulpX6AvL404DP+/iCgW9eFa2BqztiwTexswJo2jLMue" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+    {{-- Bootstrap --}}
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
