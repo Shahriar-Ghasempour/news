ffe4dd00a771681bc2a6ae18f71875b47bad1b17
shahriaarrr <shahriaarrr@gmail.com>
2025-06-28 18:16:07 +0330

    Author dashboard controllers

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,4 +11,58 @@ class PostController extends Controller
     {
         return view('post', compact(['post']));
     }
+
+    public function indexAuthor()
+    {
+        $user = auth()->user();
+
+        $posts = $user->posts;
+
+        return view('dashboard.posts', compact(['posts']));
+    }
+
+    public function create()
+    {
+        return view('dashboard.create-post');
+    }
+
+    public function store(Request $request)
+    {
+        $user = auth()->user();
+
+        $validated = $request->validate([
+            "name" => "required|max:255",
+            "body" => "required",
+            "category_id" => "nullable,exists:categories,id",
+        ]);
+
+        $post = $user->posts()->create($validated);
+
+        return redirect(route('dashboard.posts'))->with('success', 'با موفقیت ساخته شد!');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('dashboard.edit-post', compact('post'));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $validated = $request->validate([
+            'name' => 'sometimes|max:255',
+            'body' => 'sometimes',
+            "category_id" => "sometimes,exists:categories,id",
+        ]);
+
+        $post->update($validated);
+
+        return redirect(route('dashboard.posts'))->with('success', 'با موفقیت اپدیت شد!');
+    }
+
+    public function delete(Post $post)
+    {
+        $post->delete();
+
+        return redirect(route('dashboard.posts'))->with('success', 'با موفقیت حذف شد!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use App\Http\Middleware\Auth;
+use App\Http\Middleware\Author;
 use App\Http\Middleware\Guest;
 use App\Models\Category;
 use App\Models\Post;
@@ -40,11 +42,24 @@ Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
 Route::middleware([Guest::class])->prefix('/auth')->group(function() {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.show');
     Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.show');
+
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::middleware([Auth::class])->prefix('dashboard')->group(function() {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::middleware([Author::class])->group(function() {
+        Route::get('/posts')->name('dashboard.posts');
+
+        Route::get('/posts/new')->name('dashboard.create-post');
+
+        Route::get('/posts/{post}')->name('dashboard.edit-post');
+    });
 });
+
+
